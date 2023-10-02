75369860108f49bfad9a9a9f448afb23305388fe
Brock Brown <brockrbrown@gmail.com>
2023-10-02 09:32:45 -0400

    Bookmark button appears, and now console.logs 'Clicked!' when clicked.

--- contentScript.js
+++ contentScript.js
@@ -16,6 +16,39 @@
                 currentVideo = videoId;
                 newVideoLoaded();
             }
-        })
+        });
+
+        const newVideoLoaded = () => {
+            const bookmarkButtonExists = document.getElementsByClassName("bookmark-btn")[0];
+
+            console.log(bookmarkButtonExists);
+
+            if (!bookmarkButtonExists) {
+                const bookmarkButton = document.createElement("img");
+
+                bookmarkButton.src = chrome.runtime.getURL("assets/bookmark.png");
+                bookmarkButton.className = "ytp-button " + "bookmark-btn";
+                bookmarkButton.title = "Click to bookmark current timestamp";
+
+                youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+                youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+                youtubeLeftControls.appendChild(bookmarkButton);
+                bookmarkButton.addEventListener("click", addNewBookmarkHandler);
+            }
+        }
+
+        const addNewBookmarkHandler = (e) => {
+            console.log("clicked!")
+        }
+        // IMPORTANT: this calls newVideoLoaded any time our content
+        // script matches youtube.com.
+        // This fixes the problem we have where if we refresh,
+        // we're not going to see the change we made.
+        // Downside: if the background script sees it as a new
+        // video using that onUpdated event listener, we're gonna call
+        // this twice. You can fix this using a conditional to make
+        // sure this doesn't happen.
+        newVideoLoaded();
     }
 )();
