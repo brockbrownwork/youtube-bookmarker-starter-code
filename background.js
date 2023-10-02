d1e80717a13be04146b668f7c54c7dfcddad83fd
Brock Brown <brockrbrown@gmail.com>
2023-10-02 08:23:40 -0400

    Getting messaging set up in background.js

--- background.js
+++ background.js
@@ -1,12 +1,17 @@
+// do this when a tab updates:
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+  // if the tab contains youtube.com/watch, you are likely
+  // watching a YouTube video
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    // grab everything in the url after the question mark
+    const queryParameters = tab.url.split("?")[1];
+    // create an object to access different parts of the query parameters
+    const urlParameters = new URLSearchParams(queryParameters);
+    // send a message through the tabs API that says we started watching
+    // a youtube video with this unique ID
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v")
+    });
+  }
+})
